a13ed6bc3aa27d716323714d4f69865ad2f84861
yasuwo <y.honjo.i@gmail.com>
2024-09-09 00:52:22 +0900

    docker

--- install/docker.sh
+++ install/docker.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Add Docker's official GPG key:
-sudo apt-get update && apt-get install -y \
+if ! type docker > /dev/null 2>&1; then
+sudo apt-get update && sudo apt-get install -y \
 ca-certificates \
 curl
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -13,7 +14,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update && apt-get install -y \
+sudo apt-get update && sudo apt-get install -y \
 docker-ce \
 docker-ce-cli \
 containerd.io \
@@ -24,3 +25,4 @@ docker-compose-plugin
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
+fi
