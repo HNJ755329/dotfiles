9b626d4977fc2795ab94ffa18dbb6444fa466d68
yasuwo <y.honjo.i@gmail.com>
2024-09-09 00:51:01 +0900

    nvim.sh

--- install/nvim.sh
+++ install/nvim.sh
@@ -1,7 +1,15 @@
-#!/usr/bin/env sh
-
+#!/usr/bin/env bash
+if ! type nvim >/dev/null 2>&1;then
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 sudo chmod u+x nvim.appimage
-mkdir -p /opt/nvim
-mv nvim.appimage /opt/nvim/nvim
-sudo ln -s /opt/nvim/nvim /usr/bin/nvim
+# mkdir -p /opt/nvim
+# mv nvim.appimage /opt/nvim/nvim
+# sudo ln -s /opt/nvim/nvim /usr/bin/nvim
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+
+# Optional: exposing nvim globally.
+sudo mv squashfs-root /
+sudo rm /usr/bin/nvim
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+fi
