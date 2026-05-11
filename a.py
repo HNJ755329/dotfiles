951a2c504539cde5a2667e009e67bff3e78b15fd
yasuwo <y.honjo.i@gmail.com>
2026-05-11 23:31:01 +0900

    add archive folder and move some settings

---
+++ a.py
@@ -0,0 +1,13 @@
+s = f"        - {src: '{ {path} }/.config/zsh', dest: '{ {ansible_env.HOME} }/.config/zsh' }"
+alacritty
+clangd
+emacs
+git
+gwq
+i3
+nvim
+sway
+tmux
+waybar
+zsh
+cargo - atcoder.toml
